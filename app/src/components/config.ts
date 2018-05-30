cd757951c8b237a9c948e8e84e0aebb0d661e96f
Saravana <softsaravanacse@gmail.com>
2018-05-30 21:31:41 +0800

    Preparing for testing

---
+++ app/src/components/config.ts
@@ -0,0 +1,7 @@
+import * as fs from 'fs';
+import { configFile } from './constants';
+
+// only on dev
+
+// the config file can be replaced by
+// export const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
